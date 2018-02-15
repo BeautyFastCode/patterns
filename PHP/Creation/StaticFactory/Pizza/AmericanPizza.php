eb14db6427eae5e982ad36493d52d6d435ca69a9
BeautyFastCode.com <beautyfastcode@gmail.com>
2018-02-15 15:13:58 +0100

    adding Static Factory

---
+++ PHP/Creation/StaticFactory/Pizza/AmericanPizza.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * (c) BeautyFastCode.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PHP\Creation\StaticFactory\Pizza;
+
+/**
+ * AmericanPizza
+ *
+ * @author    Bogumił Brzeziński <beautyfastcode@gmail.com>
+ * @copyright BeautyFastCode.com
+ */
+class AmericanPizza extends Pizza
+{
+    /**
+     * Pizza constructor
+     */
+    public function __construct()
+    {
+        parent::__construct(PizzaTypes::AMERICAN);
+        $this->setPrice(5.0);
+    }
+}
